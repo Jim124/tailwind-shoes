b1e21773033a9e397b90af0991593bbc91872440
Jim124 <fengpingan065@gmail.com>
2024-06-24 16:02:55 +0800

    style nav compoment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,11 @@
+import { TbShoppingBag } from 'react-icons/tb';
 import NickLogo from '../assets/nike-logo.svg?react';
 import { RxHamburgerMenu } from 'react-icons/rx';
+import { useState } from 'react';
 
 const ROUTES = ['Home', 'About', 'Services', 'Pricing', 'Contact'];
 export function Nav() {
+  const [isMobileMenuShown, setIsMobileMenuShown] = useState(false);
   return (
     <nav className='flex flex-wrap justify-between items-center'>
       {/* logo */}
@@ -10,21 +13,32 @@ export function Nav() {
         <NickLogo className=' h-20 w-20' />
       </a>
       {/* burger button */}
-      <button className='hover:bg-gray-100 p-2 focus:ring-2 focus:ring-gray-200  rounded-lg'>
+      <button
+        onClick={() => setIsMobileMenuShown(!isMobileMenuShown)}
+        className='hover:bg-gray-100 p-2 focus:ring-2 focus:ring-gray-200  rounded-lg lg:hidden'
+      >
         <RxHamburgerMenu size={25} />
       </button>
       {/* menu list */}
-      <div className='w-full'>
-        <ul className='bg-gray-50 text-lg border border-gray-100 rounded-lg p-4'>
+      <div
+        className={`${!isMobileMenuShown && 'hidden'} w-full lg:w-auto lg:block`}
+      >
+        <ul className='bg-gray-50 lg:bg-transparent text-lg border border-gray-100 lg:border-none rounded-lg p-4 flex flex-col lg:flex-row lg:space-x-8  '>
           {ROUTES.map((route, index) => (
             <li
-              className={`rounded cursor-pointer px-3 py-2 mb-1 ${index === 0 ? 'bg-blue-500 text-white' : 'hover:bg-gray-100'}`}
+              className={`rounded cursor-pointer px-3 py-2 mb-1 ${index === 0 ? 'bg-blue-500 text-white lg:bg-transparent lg:text-blue-500' : 'hover:bg-gray-100'}`}
               key={route}
             >
               {route}
             </li>
           ))}
         </ul>
+      </div>
+      {/* cart button */}
+      <div className='fixed left-4 bottom-4 lg:static'>
+        <div className='flex-center rounded-full h-12 w-12 bg-white shadow-md '>
+          <TbShoppingBag />
+        </div>
       </div>
     </nav>
   );
